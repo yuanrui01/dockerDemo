aaa8d6ad8cb77911ac85e9a9b8d2676a9fcca229
yuanrui <xdyrfree@gmail.com>
2023-06-19 13:08:49 +0800

    swagger转换openapi测试

--- src/main/java/com/yuanrui/docker_boot/openapi/impl/SwaggerServiceImpl.java
+++ src/main/java/com/yuanrui/docker_boot/openapi/impl/SwaggerServiceImpl.java
@@ -3,10 +3,12 @@ package com.yuanrui.docker_boot.openapi.impl;
 import io.swagger.parser.OpenAPIParser;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
+import javafx.scene.chart.ScatterChart;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -15,20 +17,27 @@ import java.nio.charset.StandardCharsets;
  * @email: xdyrfree@gmail.com
  * @date: 2023/6/14-20:01
  */
+@Slf4j
 public class SwaggerServiceImpl {
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 /*
         SwaggerParseResult swaggerParseResult = new OpenAPIParserrser().readLocation("", null, null);
 */
         String content = FileUtils.readFileToString(
-                new File("src/main/resources/APIMarket.swagger.json"), StandardCharsets.UTF_8);
+                new File("src/main/resources/swagger测试1-8.json"), StandardCharsets.UTF_8);
 
         SwaggerParseResult swaggerParseResult = new OpenAPIParser().readContents(content, null, null);
-
         OpenAPI openAPI = swaggerParseResult.getOpenAPI();
 
-        System.out.println(content);
+        try(FileOutputStream     fileOutputStream = new FileOutputStream(new File("openAPI_output.txt"));
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(openAPI.toString().getBytes(StandardCharsets.UTF_8));
+        ){
+            IOUtils.copy(new ByteArrayInputStream(openAPI.toString().getBytes(StandardCharsets.UTF_8)), fileOutputStream);
+        }catch (Exception e){
+            e.printStackTrace();
+            log.error("粗大事啦！");
+        }
     }
 }
